c666066cd091a4d3f27d63b9204f9b6492141d00
NordTort <41715818+NorthTort@users.noreply.github.com>
2019-03-10 01:24:54 +0300

    Create Sphinx.js

---
+++ Sphinx.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+var prefix = '*'
+ 
+ client.on('message', message => {
+    if(message.author === client.user) return;
+ 	 if(message.content.startsWith(prefix + 'say')) {
+  if(message.author.id !== '386834372466049024') return;
+    const args = message.content.slice(prefix.length).split('say'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
+	 }
+	  if(message.content.startsWith(prefix + 'send')) {
+  if(message.author.id !== '435498881040777231') return;
+    const args = message.content.slice(prefix.length).split('send'); 
+	  message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('send'));
+	 }
+ 	// Арты победителя
+if(message.content.startsWith(prefix + 'question')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.png'
+      }
+   },
+   files: [{
+      attachment: 'https://downloader.disk.yandex.ru/preview/9b68968f810d893d268285ec5f4c6fe5ba78bd24ce43952aa21f01ce6f67e08c/5c846bb8/_B0aXmp4RJTYYcc2mgnKlgD9fWRLaVLvfrH0I6FQitupboeV_7fXH6qfEf143CKsvQey7-8AxTpIu0FLFbFNdA%3D%3D?uid=0&filename=Hafiz.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'question.png'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+
+client.login(process.env.BOT_TOKEN);
