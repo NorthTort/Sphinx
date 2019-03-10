b86f91acb6ee9867a401aca4c3326fb54e45b120
NordTort <41715818+NorthTort@users.noreply.github.com>
2019-03-10 10:57:16 +0300

    Update Сфинкс.js

--- Сфинкс.js
+++ Сфинкс.js
@@ -6,28 +6,52 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var prefix = '*'
+var prefix = '!'
  
  client.on('message', message => {
     if(message.author === client.user) return;
-      if(message.content.startsWith(prefix + 'say')) {
+    if(message.content.startsWith(prefix + 'ek')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+		message.channel.sendMessage('<:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> @everyone <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> \n Идёт набор на ежедневную катку!');
+	 }
+	 if(message.content.startsWith(prefix + 'turnir')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+		message.channel.sendMessage('<:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> @everyone <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> \n Идёт набор на турнир! Успей встать в очередь на поражение!');
+	 }
+	  if(message.content.startsWith(prefix + 'kubek')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+		message.channel.sendMessage('<:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> @everyone <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> \n Граждане Имперского города! Делайте ваши ставки! Кто победит в ежедневной катке!? \n Кликайте по иконке претендента!');
+	 }
+	 if(message.content.startsWith(prefix + 'kubtur')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+		message.channel.sendMessage('<:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> @everyone <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> <:KolesoUdachi:469348898058797058> \n Граждане Имперского города! Делайте ваши ставки! Кто победит в сегодняшнем турнире!? \n Кликайте по иконке претендента!');
+	 }	 
+	 if(message.content.startsWith(prefix + 'say')) {
   if(message.author.id !== '386834372466049024') return;
     const args = message.content.slice(prefix.length).split('say'); 
-      message.delete()
+	  message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error); 
-             message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
-     }
-      if(message.content.startsWith(prefix + 'send')) {
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('say'));
+	 }
+	  if(message.content.startsWith(prefix + 'send')) {
   if(message.author.id !== '435498881040777231') return;
     const args = message.content.slice(prefix.length).split('send'); 
-      message.delete()
+	  message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error); 
-             message.channel.sendMessage(message.content.slice(prefix.length).split('send'));
-     }
-     // Арты победителя
-if(message.content.startsWith(prefix + 'question')) {
+     		message.channel.sendMessage(message.content.slice(prefix.length).split('send'));
+	 }
+ 	// Арты победителя
+if(message.content.startsWith(prefix + 'hero')) {
         message.delete()
   .then(msg => console.log(`Deleted message from ${msg.author.username}`))
   .catch(console.error); 
@@ -38,12 +62,69 @@ message.channel.send({
       }
    },
    files: [{
-      attachment: '',
-      name: 'question.png'
+      attachment: 'https://downloader.disk.yandex.ru/preview/9b68968f810d893d268285ec5f4c6fe5ba78bd24ce43952aa21f01ce6f67e08c/5c846bb8/_B0aXmp4RJTYYcc2mgnKlgD9fWRLaVLvfrH0I6FQitupboeV_7fXH6qfEf143CKsvQey7-8AxTpIu0FLFbFNdA%3D%3D?uid=0&filename=Hafiz.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&tknv=v2&size=2048x2048',
+      name: 'Win Hero.png'
    }]
 })
 .then(console.log)
 .catch(console.error);
 }
+//Карта
+if(message.content.startsWith(prefix + 'map')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.h5m'
+      }
+   },
+   files: [{
+      attachment: 'https://github.com/NorthTort/Valeria/raw/master/Tri_Dorogi.h5m',
+      name: 'Tri Dorogi.h5m'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+//Сражение
+if(message.content.startsWith(prefix + 'battle')) {
+        message.delete()
+  .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+  .catch(console.error); 
+message.channel.send({
+  embed: {
+    thumbnail: {
+         url: 'attachment://file.sav'
+      }
+   },
+   files: [{
+      attachment: 'https://github.com/NorthTort/Valeria/raw/master/Salo1221_VS_Scorpio.sav',
+      name: 'Salo1221_VS_Scorpio.sav'
+   }]
+})
+.then(console.log)
+.catch(console.error);
+}
+
+let messageArray = message.content.split(" ");
+let args = messageArray.slice(1);    
+    if(message.content.startsWith(prefix + 'calc')) {
+const sayMessage1 = args.join(" ");
+        message.channel.send(eval(sayMessage1))
+     } 
+ 
+ });
+
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('432864200864301059').send('<:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> **' + member.user + '**, приветствую тебя, Воин! <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> <:Liderstvo:456581505066729474> \n Прочти правила сервера, чтобы не налажать: <#464435496773484554>.'); 
+	});
+
+
+client.on('guildMemberRemove', member => {
+    member.guild.channels.get('432864200864301059').send('<:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> **' + member.user + '**, беги! Жалкий трус! <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778> <:Nadzor:460625006045429778>');
+    // channel-id - Надо вводить ид канала куда будет писаться это сообщение
+});
 
 client.login(process.env.BOT_TOKEN);
