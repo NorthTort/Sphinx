663a51968c15c9770f6a9584802f06ff4e1d5ef1
NordTort <41715818+NorthTort@users.noreply.github.com>
2019-05-15 23:29:07 +0300

    Update Сфинкс.js

--- Сфинкс.js
+++ Сфинкс.js
@@ -63,7 +63,7 @@ message.channel.send({
       }
    },
    files: [{
-      attachment: 'https://github.com/NorthTort/Sphinx/raw/master/Б1112.png',
+      attachment: 'https://github.com/NorthTort/Sphinx/raw/master/Vopros15.png',
       name: 'Win Hero.png'
    }]
 })
